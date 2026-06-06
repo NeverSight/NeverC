d524c7b25738f081e58d963c02d2e963e169d5d0
NeverSightAI <neversightai@gmail.com>
2026-06-06 14:28:33 +0800

    Refactor compiler compatibility macros in CompilerCompat.h
    - Updated prefetch macro definitions to improve compatibility between GCC/Clang and MSVC.
    - Removed redundant conditional checks and streamlined the inclusion of compiler-specific intrinsics.
    - Ensured proper handling of lambda inline attributes across different compilers.

--- neverc/include/neverc/Foundation/Core/CompilerCompat.h
+++ neverc/include/neverc/Foundation/Core/CompilerCompat.h
@@ -5,7 +5,11 @@
 /// Cross-compiler compatibility macros for intrinsics that differ between
 /// GCC/Clang and MSVC.
 
-#ifdef _MSC_VER
+#if defined(__clang__) || defined(__GNUC__)
+
+#define NEVERC_PREFETCH(addr, rw, locality) __builtin_prefetch(addr, rw, locality)
+
+#elif defined(_MSC_VER)
 #include <intrin.h>
 
 #if defined(_M_ARM64) || defined(_M_ARM64EC)
@@ -25,19 +29,14 @@
                NEVERC_PREFETCH_LOCALITY_##locality)
 
 #endif // _M_ARM64
-
-#else // GCC/Clang
-
-#define NEVERC_PREFETCH(addr, rw, locality) __builtin_prefetch(addr, rw, locality)
-
-#endif // _MSC_VER
+#endif // __clang__ || __GNUC__ / _MSC_VER
 
 /// Lambda-compatible always_inline. Unlike LLVM_ATTRIBUTE_ALWAYS_INLINE, omits
 /// the \c inline keyword which is invalid on lambda expressions.
-#ifdef _MSC_VER
-#define NEVERC_LAMBDA_ALWAYS_INLINE
-#else
+#if defined(__clang__) || defined(__GNUC__)
 #define NEVERC_LAMBDA_ALWAYS_INLINE __attribute__((always_inline))
+#else
+#define NEVERC_LAMBDA_ALWAYS_INLINE
 #endif
 
 #endif // NEVERC_FOUNDATION_CORE_COMPILERCOMPAT_H
