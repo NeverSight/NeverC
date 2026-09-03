02e4a6122c4ecda53c3dae1717e34bcaa85987f2
NeverSightAI <neversightai@gmail.com>
2026-09-04 00:10:59 +0800

    test(std): require complete bufio dot methods

--- utils/lint/test_gen_std_module_methods.py
+++ utils/lint/test_gen_std_module_methods.py
@@ -181,6 +181,18 @@ class GenStdModuleMethodsTest(unittest.TestCase):
             "flag has registered symbols without dot methods",
         )
 
+    def test_bufio_module_maps_every_registered_symbol(self):
+        with GEN.DEFAULT_MANIFEST.open(encoding="utf-8") as source:
+            module = json.load(source)["modules"]["bufio"]
+
+        registered = set(module["symbols"])
+        mapped = set(module.get("dot_methods", {}).values())
+        self.assertEqual(
+            registered - mapped,
+            set(),
+            "bufio has registered symbols without dot methods",
+        )
+
     def test_check_mode_detects_a_stale_file(self):
         manifest = {
             "modules": {
