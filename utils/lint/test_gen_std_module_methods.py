86f892d9cc142f690027b4bf37687603566502b4
NeverSightAI <neversightai@gmail.com>
2026-09-03 08:54:53 +0800

    test(std): require complete html template dot methods

--- utils/lint/test_gen_std_module_methods.py
+++ utils/lint/test_gen_std_module_methods.py
@@ -133,6 +133,18 @@ class GenStdModuleMethodsTest(unittest.TestCase):
             "image has registered symbols without dot methods",
         )
 
+    def test_html_template_module_maps_every_registered_symbol(self):
+        with GEN.DEFAULT_MANIFEST.open(encoding="utf-8") as source:
+            module = json.load(source)["modules"]["html/template"]
+
+        registered = set(module["symbols"])
+        mapped = set(module.get("dot_methods", {}).values())
+        self.assertEqual(
+            registered - mapped,
+            set(),
+            "html/template has registered symbols without dot methods",
+        )
+
     def test_check_mode_detects_a_stale_file(self):
         manifest = {
             "modules": {
