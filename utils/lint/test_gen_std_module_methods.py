071fea34a260efb07c8bf2c634d8452ab604ac9f
NeverSightAI <neversightai@gmail.com>
2026-09-04 05:48:10 +0800

    test(std): require complete cmp dot methods

--- utils/lint/test_gen_std_module_methods.py
+++ utils/lint/test_gen_std_module_methods.py
@@ -193,6 +193,18 @@ class GenStdModuleMethodsTest(unittest.TestCase):
             "bufio has registered symbols without dot methods",
         )
 
+    def test_cmp_module_maps_every_registered_symbol(self):
+        with GEN.DEFAULT_MANIFEST.open(encoding="utf-8") as source:
+            module = json.load(source)["modules"]["cmp"]
+
+        registered = set(module["symbols"])
+        mapped = set(module.get("dot_methods", {}).values())
+        self.assertEqual(
+            registered - mapped,
+            set(),
+            "cmp has registered symbols without dot methods",
+        )
+
     def test_check_mode_detects_a_stale_file(self):
         manifest = {
             "modules": {
